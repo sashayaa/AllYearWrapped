51404aacdd7532fd3a26fdcbba7fd8c9c6adf436
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2023-07-25 22:56:59 +0100

    Add jest for typescript and fix clashing with cypress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'AllYear Wrapped',
+  title: 'AllYearWrapped',
   description: 'Explore Spotify Wrapped all year long',
 }
 
